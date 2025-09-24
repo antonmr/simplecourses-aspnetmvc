a121c9985c52d52e8219bb167dba7674df42713e
Anton Rangel <amazonrangel@gmail.com>
2025-09-23 21:45:25 -0300

    Added Custom Login Dialog

---
+++ SimpleCourses/Controllers/UserAuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SimpleCourses.Data;
+using SimpleCourses.Models;
+
+namespace SimpleCourses.Controllers
+{
+    public class UserAuthController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public UserAuthController(UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            loginModel.LoginInvalid = "true";
+
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginModel.Email, loginModel.Password, 
+                    loginModel.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    loginModel.LoginInvalid = string.Empty;
+                } else
+                {
+                    loginModel.RememberMe = false;
+                    ModelState.AddModelError(string.Empty, "Invalid attempt.");
+                }
+            }
+
+            return PartialView("_UserLoginPartial", loginModel);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+
+            if(returnUrl is not null)
+            {
+                return LocalRedirect(returnUrl);
+            } else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ SimpleCourses/wwwroot/js/userlogin.js
@@ -0,0 +1,39 @@
+﻿$(function () {
+    function onUserLoginClick() {
+        var url = 'UserAuth/Login';
+
+        var userInput = {
+            __RequestVerificationToken: $("#UserLoginModal input[name='__RequestVerificationToken']").val(),
+            Email: $('#UserLoginModal #Email').val(),
+            Password: $('#UserLoginModal #Password').val(),
+            RememberMe: $('#UserLoginModal #RememberMe').val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: userInput,
+            success: function (data) { // callback method
+                var parsed = $.parseHTML(data);
+
+                var hasErrors = $(parsed).find("input[name='LoginInvalid']").val() == 'true';
+                if (hasErrors) {
+                    $('#UserLoginModal').html(data);
+                    userLoginButton = $('#UserLoginModal #login').click(onUserLoginClick);
+
+                    var form = $('#UserLoginForm');
+                    $(form).removeData("validator");
+                    $(form).removeData("unobtrusiveValidation");
+                    $.validator.unobtrusive.parse(form);
+                } else {
+                    location.href = "Home/Index";
+                }
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.error(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+            }
+        });
+    }
+
+    var userLoginButton = $('#UserLoginModal #login').click(onUserLoginClick);
+});
